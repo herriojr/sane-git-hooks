b765937376de7e2ff83ba7d17ddc072ac144902e
Jonathan Herriott <jherriott@bitcasa.com>
2014-06-16 12:50:52 -0700

    Fixed problem where it wasn't actually exiting when a sub-script was asking to exit.

--- hooks/generic/defer_to_folders.sh
+++ hooks/generic/defer_to_folders.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 THIS_DIR=`dirname $0`
-SUBDIR="$1"
+SUBDIR="$0-dir"
 
 for FILE in `ls -R $SUBDIR`; do
-	FILE_PATH=$SUBDIR/$FILE
-	if [ -f "$FILE_PATH" ]; then
-		$SUBDIR/$FILE
-	fi
+        FILE_PATH=$SUBDIR/$FILE
+
+        if [ -f "$FILE_PATH" ]; then
+                $SUBDIR/$FILE
+                result=$?
+                if [ result != 0 ]; then
+                        exit $result
+                fi
+        fi
 done
